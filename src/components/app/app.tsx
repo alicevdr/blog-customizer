5303c2dd2ad8bfd21b8ef1d8064eda4eaa877427
alicedvr <alisa-muzica@yandex.ru>
2026-09-06 15:23:41 +0300

    Первый коммит по ПР№10

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,26 +1,33 @@
-import { CSSProperties } from 'react';
+import { useState, CSSProperties } from 'react';
 import clsx from 'clsx';
-
 import { Article } from '../article/Article';
 import { ArticleParamsForm } from '../article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './../../constants/articleProps';
-
+import {
+	defaultArticleState,
+	type ArticleStateType,
+} from '../../constants/articleProps';
 import styles from './app.module.scss';
 
 export const App = () => {
+	const [appState, setAppState] =
+		useState<ArticleStateType>(defaultArticleState);
+
 	return (
 		<main
 			className={clsx(styles.main)}
 			style={
 				{
-					'--font-family': defaultArticleState.fontFamilyOption.value,
-					'--font-size': defaultArticleState.fontSizeOption.value,
-					'--font-color': defaultArticleState.fontColor.value,
-					'--container-width': defaultArticleState.contentWidth.value,
-					'--bg-color': defaultArticleState.backgroundColor.value,
+					'--font-family': appState.fontFamilyOption.value,
+					'--font-size': appState.fontSizeOption.value,
+					'--font-color': appState.fontColor.value,
+					'--container-width': appState.contentWidth.value,
+					'--bg-color': appState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				currentState={appState}
+				setCurrentState={setAppState}
+			/>
 			<Article />
 		</main>
 	);
